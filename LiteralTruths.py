e4f618cfb9b182239f023dabd1421777b254a7f9
Makaylamckay9999 <nicolemckay4320@gmail.com>
2026-09-16 21:01:34 -0500

    Correction
    Corrected first statement

--- LiteralTruths.py
+++ LiteralTruths.py
@@ -1,4 +1,4 @@
-print("My One of my favorite bands is TXT!") #String Literal
+print("One of my favorite bands is TXT!") #String Literal
 print (333) #Integer literal
 print (True) #I'm unable to whistle 
 print (4.19) #Float literal
