23567467ec1f23e66e0c9b72d0a8e34f17533a49
Makaylamckay9999 <nicolemckay4320@gmail.com>
2026-09-16 21:00:24 -0500

    Void
    Added Void literal and corrected comments

--- LiteralTruths.py
+++ LiteralTruths.py
@@ -1,4 +1,5 @@
 print("My One of my favorite bands is TXT!") #String Literal
-print (333) #Integer
+print (333) #Integer literal
 print (True) #I'm unable to whistle 
-print (4.19) #Float
+print (4.19) #Float literal
+print (None) #Void literal
